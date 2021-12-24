773b15a1dc2e7e774fd4f69e1145254a7db0b9b6
Thai-dot <73903817+Thai-dot@users.noreply.github.com>
2021-12-24 08:15:12 +0700

    Add files via upload

---
+++ KhachHang/KhachHang/DangKy_KH.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace KhachHang
+{
+    partial class DangKy_KH
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tb_TenDN_DK = new System.Windows.Forms.TextBox();
+            this.tb_MK_DK = new System.Windows.Forms.TextBox();
+            this.tb_NLMK_TK = new System.Windows.Forms.TextBox();
+            this.tb_HoTen_DK = new System.Windows.Forms.TextBox();
+            this.tb_SDT_DK = new System.Windows.Forms.TextBox();
+            this.tb_DiaChi_KH = new System.Windows.Forms.TextBox();
+            this.tb_Email_DK = new System.Windows.Forms.TextBox();
+            this.btn_dangky_kh = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(288, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đăng Ký Tài Khoản";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên Đăng Nhập:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(111, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mật Khẩu:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(111, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nhập Lại Mật Khẩu:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(111, 234);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Họ Tên của bạn:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(111, 275);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(105, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Số Điện Thoại:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(111, 312);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Địa Chỉ:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(111, 358);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Email:";
+            // 
+            // tb_TenDN_DK
+            // 
+            this.tb_TenDN_DK.Location = new System.Drawing.Point(288, 103);
+            this.tb_TenDN_DK.Name = "tb_TenDN_DK";
+            this.tb_TenDN_DK.Size = new System.Drawing.Size(316, 27);
+            this.tb_TenDN_DK.TabIndex = 8;
+            // 
+            // tb_MK_DK
+            // 
+            this.tb_MK_DK.Location = new System.Drawing.Point(288, 149);
+            this.tb_MK_DK.Name = "tb_MK_DK";
+            this.tb_MK_DK.Size = new System.Drawing.Size(316, 27);
+            this.tb_MK_DK.TabIndex = 9;
+            this.tb_MK_DK.UseSystemPasswordChar = true;
+            // 
+            // tb_NLMK_TK
+            // 
+            this.tb_NLMK_TK.Location = new System.Drawing.Point(288, 189);
+            this.tb_NLMK_TK.Name = "tb_NLMK_TK";
+            this.tb_NLMK_TK.Size = new System.Drawing.Size(316, 27);
+            this.tb_NLMK_TK.TabIndex = 10;
+            this.tb_NLMK_TK.UseSystemPasswordChar = true;
+            // 
+            // tb_HoTen_DK
+            // 
+            this.tb_HoTen_DK.Location = new System.Drawing.Point(288, 234);
+            this.tb_HoTen_DK.Name = "tb_HoTen_DK";
+            this.tb_HoTen_DK.Size = new System.Drawing.Size(316, 27);
+            this.tb_HoTen_DK.TabIndex = 11;
+            // 
+            // tb_SDT_DK
+            // 
+            this.tb_SDT_DK.Location = new System.Drawing.Point(288, 275);
+            this.tb_SDT_DK.Name = "tb_SDT_DK";
+            this.tb_SDT_DK.Size = new System.Drawing.Size(316, 27);
+            this.tb_SDT_DK.TabIndex = 12;
+            // 
+            // tb_DiaChi_KH
+            // 
+            this.tb_DiaChi_KH.Location = new System.Drawing.Point(288, 312);
+            this.tb_DiaChi_KH.Name = "tb_DiaChi_KH";
+            this.tb_DiaChi_KH.Size = new System.Drawing.Size(316, 27);
+            this.tb_DiaChi_KH.TabIndex = 13;
+            // 
+            // tb_Email_DK
+            // 
+            this.tb_Email_DK.Location = new System.Drawing.Point(288, 355);
+            this.tb_Email_DK.Name = "tb_Email_DK";
+            this.tb_Email_DK.Size = new System.Drawing.Size(316, 27);
+            this.tb_Email_DK.TabIndex = 14;
+            // 
+            // btn_dangky_kh
+            // 
+            this.btn_dangky_kh.Location = new System.Drawing.Point(510, 416);
+            this.btn_dangky_kh.Name = "btn_dangky_kh";
+            this.btn_dangky_kh.Size = new System.Drawing.Size(94, 29);
+            this.btn_dangky_kh.TabIndex = 15;
+            this.btn_dangky_kh.Text = "Đăng Ký ";
+            this.btn_dangky_kh.UseVisualStyleBackColor = true;
+            this.btn_dangky_kh.Click += new System.EventHandler(this.btn_dangky_kh_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(288, 416);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Trở Về";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseWaitCursor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // DangKy_KH
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 484);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_dangky_kh);
+            this.Controls.Add(this.tb_Email_DK);
+            this.Controls.Add(this.tb_DiaChi_KH);
+            this.Controls.Add(this.tb_SDT_DK);
+            this.Controls.Add(this.tb_HoTen_DK);
+            this.Controls.Add(this.tb_NLMK_TK);
+            this.Controls.Add(this.tb_MK_DK);
+            this.Controls.Add(this.tb_TenDN_DK);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "DangKy_KH";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DangKy_KH";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button btn_dangky_kh;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtTenDN_DK;
+        private System.Windows.Forms.TextBox txtMK_DK;
+        private System.Windows.Forms.TextBox txt_NLMK_TK;
+        private System.Windows.Forms.TextBox txt_HoTen_DK;
+        private System.Windows.Forms.TextBox txt_SDT_DK;
+        private System.Windows.Forms.TextBox txt_DiaChi_KH;
+        private System.Windows.Forms.TextBox txt_Email_DK;
+        private System.Windows.Forms.TextBox tb_TenDN_DK;
+        private System.Windows.Forms.TextBox tb_MK_DK;
+        private System.Windows.Forms.TextBox tb_NLMK_TK;
+        private System.Windows.Forms.TextBox tb_HoTen_DK;
+        private System.Windows.Forms.TextBox tb_SDT_DK;
+        private System.Windows.Forms.TextBox tb_DiaChi_KH;
+        private System.Windows.Forms.TextBox tb_Email_DK;
+    }
+}
